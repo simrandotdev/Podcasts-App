f36f8c0801f744c8b8c426e29da9a22103e167db
simransdsu <simran5590@gmail.com>
2018-05-15 14:00:49 -0700

    Initial Commit

---
+++ Podcasts App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Podcasts App
+//
+//  Created by Simran Singh on 5/15/18.
+//  Copyright © 2018 Simran App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
