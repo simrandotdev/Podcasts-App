140e6970d5b124d1d2a9fd4afd683d7715f03099
simransdsu <simran5590@gmail.com>
2018-06-15 12:53:49 -0700

    FavoriteRepository created

---
+++ Podcasts App/Repositories/FavoritePodcastRepository.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+class FavoritePodcastRepository
+{
+    func favoritePodcast(podcast: Podcast)
+    {
+        guard var favoritePodcasts = fetchFavoritePodcasts() else { return }
+        favoritePodcasts.append(podcast)
+        let favoritePodcastsData = NSKeyedArchiver.archivedData(withRootObject: favoritePodcasts)
+        UserDefaults.standard.setValue(favoritePodcastsData, forKey: "favoritePodcasts")
+    }
+    
+    func unfavoritePodcast(podcast: Podcast)
+    {
+        guard var favoritePodcasts = fetchFavoritePodcasts() else { return }
+        let indexToDelete = indexOfPodcastToDelete(podcast: podcast, from: favoritePodcasts)
+        if let indexToDelete = indexToDelete,
+            indexToDelete >= 0 {
+            favoritePodcasts?.remove(at: indexToDelete)
+        }
+    }
+    
+    fileprivate
+    func fetchFavoritePodcasts() -> [Podcast]?
+    {
+        let podcastsData = UserDefaults.standard.data(forKey: "favoritePodcasts")
+        let favoritePodcasts = NSKeyedUnarchiver.unarchiveObject(with: podcastsData ?? Data()) as? [Podcast]
+        return favoritePodcasts
+    }
+    
+    fileprivate
+    func indexOfPodcastToDelete(podcast: Podcast, from podcasts: [Podcast]) -> Int?
+    {
+        guard let podcasts = podcasts else { return -1 }
+        return podcasts.index(of: podcast)
+    }
+}
