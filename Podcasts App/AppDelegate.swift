bb7f90c3d9057fc583edd39bd79f66c4a212f4dd
simransdsu <simran5590@gmail.com>
2018-09-14 09:10:37 -0700

    Navigation and SearchBar Customized a Little

--- Podcasts App/AppDelegate.swift
+++ Podcasts App/AppDelegate.swift
@@ -1,5 +1,11 @@
 import UIKit
 
+let primaryColor = UIColor(red: 0.15, green: 0.20, blue: 0.22, alpha: 1.0);
+let primaryLightColor = UIColor(red: 0.31, green: 0.36, blue: 0.38, alpha: 1.0);
+let primaryDarkColor = UIColor(red: 0.00, green: 0.04, blue: 0.07, alpha: 1.0);
+let primaryDarkTextColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.0);
+let primaryLightTextColor = UIColor.white
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
@@ -12,6 +18,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate
         window?.makeKeyAndVisible()
         window?.rootViewController = MainTabBarController()
         
+        setupAppUI()
+        
         return true
+    }
+    
+    func setupAppUI() {
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().barTintColor = primaryColor
+        UINavigationBar.appearance().tintColor = primaryLightColor
+        UINavigationBar.appearance().prefersLargeTitles = true
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedStringKey.foregroundColor: primaryLightTextColor]
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSAttributedStringKey.foregroundColor: primaryLightTextColor,
+            NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 24)
+        ]
+        
+        UITabBar.appearance().tintColor = primaryDarkColor
+        
+        UISearchBar.appearance().tintColor = primaryLightTextColor
+        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = [NSAttributedStringKey.foregroundColor.rawValue: primaryLightTextColor]
     }
 }
