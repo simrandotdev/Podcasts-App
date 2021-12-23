719ad0d952bb8d68a8691b8410900e832d8078da
simransdsu <simran5590@gmail.com>
2021-12-22 18:19:05 -0800

    Updated deployment target and added an Async method to fetch podcasts in APIService

--- Podcasts App/API/APIService.swift
+++ Podcasts App/API/APIService.swift
@@ -8,6 +8,42 @@ class APIService {
     
     private init() { }
     
+    func fetchPodcastsAsync(searchText: String) async throws -> [Podcast] {
+        let BASEURL = "https://podcasts-server.herokuapp.com"
+        let searchURLString = "\(BASEURL)?query=\(searchText)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        
+        guard let url = URL(string: searchURLString) else {
+            fatalError("Failed to create a URL")
+        }
+        let request = URLRequest(url: url)
+        
+        return try await withCheckedThrowingContinuation({ continutation in
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                
+                if let _ = error {
+                    continutation.resume(throwing: APIError.invalidRequest)
+                    return
+                }
+                
+                do {
+                    guard let data = data else {
+                        continutation.resume(throwing: APIError.invalidRequest)
+                        return
+                    }
+                    
+                    let podcasts = try JSONDecoder().decode([Podcast].self, from: data)
+                    continutation.resume(returning: podcasts)
+                } catch {
+                    continutation.resume(throwing: APIError.invalidRequest)
+                }
+                
+                
+            }.resume()
+        })
+    }
+    
+    
     func fetchPodcasts(searchText: String) -> Observable<[Podcast]> {
         let BASEURL = "https://podcasts-server.herokuapp.com"
         let searchURLString = "\(BASEURL)?query=\(searchText)".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
@@ -71,4 +107,5 @@ class APIService {
 enum APIError: Error {
     case failedToParseRss
     case failedToParseJSON
+    case invalidRequest
 }
