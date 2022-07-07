0ae773d7d2893b58210cb850725b3e703c05caf8
Simran Preet Singh <simran5590@gmail.com>
2022-07-07 06:58:36 -0700

    Fixed DB creating issue

--- Podcasts App/Data Layers/Persistance/PersistanceManager.swift
+++ Podcasts App/Data Layers/Persistance/PersistanceManager.swift
@@ -11,18 +11,20 @@ import GRDB
 
 class PersistanceManager {
     public static let shared = PersistanceManager()
-
+    
     public var dbQueue: DatabaseQueue?
-
+    
     private init() {
-
-        let databaseURL = try? FileManager.default
-            .url(for: .applicationDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
-            .appendingPathComponent("db.sqlite")
-        dbQueue = try? DatabaseQueue(path: databaseURL?.path ?? "")
-
+        let databaseURL = getDatabasePath()
+        
+        if let documentDirectoryPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            info(documentDirectoryPath)
+        }
+        
+        dbQueue = try? DatabaseQueue(path: databaseURL)
+        
         try? dbQueue?.write { db in
-
+            
             try? db.create(table: "podcast") { t in
                 t.column("recordId", .text)
                 t.column("title", .text)
@@ -32,5 +34,28 @@ class PersistanceManager {
                 t.column("rssFeedUrl", .text).primaryKey()
             }
         }
+        
+    }
+    
+    
+    public func getDatabasePath() -> String {
+        
+        let databaseURL = try? FileManager.default
+            .url(for: .applicationSupportDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+            .appendingPathComponent("db.sqlite")
+        
+        // If DB file not present in file manager copy DB file to user document directory
+        let fileManager = FileManager.default
+        
+        if  let databasePath = databaseURL?.path,
+            fileManager.fileExists(atPath: databasePath) {
+            return databasePath
+        } else {
+            err("FILE NOT AVAILABLE ... Copying")
+            if fileManager.createFile(atPath: (databaseURL?.path)!, contents: Data(), attributes: nil) {
+                return (databaseURL?.path)!
+            }
+        }
+        return ""
     }
 }
